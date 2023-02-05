187b588e8e7a7a8d4094c1b73088a8760706ff65
Grif-Rift-BY <pavel.zalazaev@protonmail.com>
2023-02-05 06:03:39 +0300

    feat: add main page

--- src/pages/Main/index.ts
+++ src/pages/Main/index.ts
@@ -1,27 +1,42 @@
 import state from '../../store/state';
 import createElement from '../../utils/createElement';
+import template from './template.html';
 
 class MainPage {
   render(): void {
     document.title = 'Main Page';
+    document.body.classList.add('body');
     document.body.innerHTML = `
       <app-header></app-header>
-      <h1>Hello! Main Page</h1>
-      <app-footer></app-footer>
-    `;
-    // <login-form></login-form>
-    // try {
-    //   const main = createElement('div', document.body) as HTMLElement;
-    //   // createElement('login-form', main);
-    //   const form = document.createElement('login-form');
-    //   main.appendChild(form);
-    // } catch (error) {
-    //   console.log(error);
-    // }
+      ${template}
+      <app-footer></app-footer>`;
     if (!state.isAuthorized) {
       const main = createElement('div', document.body) as HTMLElement;
       createElement('login-form', main);
     }
+    this.renderScroll3D();
+  }
+
+  renderScroll3D(): void {
+    window.onscroll = () => {
+      const zAxisSpacing = -1000;
+      let lastPos = zAxisSpacing / 5;
+      const $frames: HTMLCollectionOf<Element> = document.getElementsByClassName('frame');
+      const frames: Element[] = Array.from($frames);
+      const zAxisVals: number[] = [];
+      const top = document.documentElement.scrollTop;
+      const delta = lastPos - top;
+      lastPos = top;
+      frames.forEach((fr, i) => {
+        zAxisVals.push((i * zAxisSpacing) + zAxisSpacing);
+        zAxisVals[i] += delta * -5.5;
+        const frame = frames[i];
+        const transform = `translateZ(${zAxisVals[i]}px)`;
+        const opacity = zAxisVals[i] < Math.abs(zAxisSpacing) / 1.8 ? 1 : 0;
+        frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`);
+      });
+    };
+    window.scrollTo(0, 1);
   }
 }
 
--- src/pages/NotFound/index.ts
+++ src/pages/NotFound/index.ts
@@ -3,6 +3,7 @@ import template from './template.html';
 class NotFoundPage {
   render(): void {
     document.title = '404 Page';
+    document.body.classList.remove('body');
     document.body.innerHTML = template;
   }
 }
