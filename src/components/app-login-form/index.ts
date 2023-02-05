10a6e7863245e4bcb9ec9abc85b020bde8b1a41c
Grif-Rift-BY <pavel.zalazaev@protonmail.com>
2023-02-06 01:55:50 +0300

    refactor: change styles in auth form to common site style

--- src/components/app-login-form/index.ts
+++ src/components/app-login-form/index.ts
@@ -1,5 +1,4 @@
 import template from './template.html';
-import './index.scss';
 import { Ilogin, IUser } from '../../types';
 import validate from '../../utils/validate';
 import getUser from '../../api';
@@ -8,10 +7,12 @@ import router from '../../router';
 
 class AppLoginForm extends HTMLElement {
   connectedCallback() {
+    console.log('AppLoginForm added');
     this.innerHTML = template;
     const form = this.querySelector('.login-form') as HTMLFormElement;
     const passEyeBtn = this.querySelector('.password-eye') as HTMLButtonElement;
     passEyeBtn.onclick = () => {
+      console.log('passEyeBtn');
       const input = passEyeBtn.nextElementSibling as HTMLInputElement;
       if (input) {
         if (input.type === 'password') {
@@ -65,25 +66,25 @@ class AppLoginForm extends HTMLElement {
   }
 
   private setInputFieldState() {
-    const inputs = this.querySelectorAll('.input');
+    const inputs = this.querySelectorAll('.input-auth');
     inputs.forEach((input) => {
       const currInput = input as HTMLInputElement;
       currInput.onblur = () => {
         const { name, value } = currInput;
         this.showMessage(currInput);
-        currInput.classList.remove('input_filled');
-        currInput.classList.remove('input_error');
+        currInput.classList.remove('input-auth_filled');
+        currInput.classList.remove('input-auth_error');
         if (currInput.value.trim() === '') {
           this.showMessage(currInput, 'Field empty');
-          currInput.classList.add('input_error');
+          currInput.classList.add('input-auth_error');
         } else {
-          currInput.classList.add('input_filled');
+          currInput.classList.add('input-auth_filled');
           console.log(name, value, validate[name](value));
           if (validate[name](value)) {
-            currInput.classList.remove('input_error');
+            currInput.classList.remove('input-auth_error');
             currInput.setAttribute('data-success', 'data-success');
           } else {
-            currInput.classList.add('input_error');
+            currInput.classList.add('input-auth_error');
             this.showMessage(currInput, 'Error value');
           }
         }
