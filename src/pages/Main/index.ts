8c0877fee3c30465db45669d2ebb972996d51634
Grif-Rift-BY <pavel.zalazaev@protonmail.com>
2023-02-06 03:50:36 +0300

    fix: add temporary iife func to method renderScroll3D()

--- src/pages/Main/index.ts
+++ src/pages/Main/index.ts
@@ -4,29 +4,47 @@ import template from './template.html';
 
 class MainPage {
   render(): void {
+    console.log('render() MainPage');
     document.title = 'Main Page';
     document.body.classList.add('body');
     document.body.innerHTML = `
       <app-header></app-header>
       ${template}
       <app-footer></app-footer>`;
-    if (!state.isAuthorized) {
-      const main = createElement('div', document.body) as HTMLElement;
-      createElement('login-form', main);
-    }
+    document.querySelector('.user-menu__login')?.addEventListener('click', () => {
+      if (!state.isAuthorized) {
+        createElement('login-form', document.body);
+        document.body.classList.add('overflow-hidden');
+      }
+    });
     this.renderScroll3D();
   }
 
   renderScroll3D(): void {
-    window.onscroll = () => {
-      const zAxisSpacing = -1000;
-      let lastPos = zAxisSpacing / 5;
-      const $frames: HTMLCollectionOf<Element> = document.getElementsByClassName('frame');
-      const frames: Element[] = Array.from($frames);
-      const zAxisVals: number[] = [];
+    console.log('renderScroll3D()');
+    const zAxisSpacing = -1000;
+    let lastPos = zAxisSpacing / 5;
+    const $frames: HTMLCollectionOf<Element> = document.getElementsByClassName('frame');
+    const frames: Element[] = Array.from($frames);
+    const zAxisVals: number[] = [];
+    (() => {
       const top = document.documentElement.scrollTop;
       const delta = lastPos - top;
       lastPos = top;
+      frames.forEach((fr, i) => {
+        zAxisVals.push((i * zAxisSpacing) + zAxisSpacing);
+        zAxisVals[i] += delta * -5.5;
+        const frame = frames[i];
+        const transform = `translateZ(${zAxisVals[i]}px)`;
+        const opacity = zAxisVals[i] < Math.abs(zAxisSpacing) / 1.8 ? 1 : 0;
+        frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`);
+      });
+    })();
+    window.onscroll = () => {
+      const top = document.documentElement.scrollTop;
+      const delta = lastPos - top;
+      lastPos = top;
+      console.log('onscroll lastPos', lastPos);
       frames.forEach((fr, i) => {
         zAxisVals.push((i * zAxisSpacing) + zAxisSpacing);
         zAxisVals[i] += delta * -5.5;
