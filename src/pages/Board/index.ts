ab22d10c899227b405baa1717bc466f2e3aa5474
Grif-Rift-BY <pavel.zalazaev@protonmail.com>
2023-02-06 21:22:53 +0300

    fix: fix some bugs

--- src/pages/Board/index.ts
+++ src/pages/Board/index.ts
@@ -9,6 +9,7 @@ class BoardPage {
     ${template}`;
     const board = document.getElementById('board');
     board!.innerHTML = '<app-status></app-status>';
+    board!.innerHTML += '<app-status></app-status>';
     const list = board!.querySelector('.status__task-list');
     list!.innerHTML = '<app-task></app-task>';
     const task = board!.querySelector('.task');
