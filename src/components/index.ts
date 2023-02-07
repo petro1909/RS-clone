8aa910cc12c2a06184d81641d3ead6162d3d7c4e
Grif-Rift-BY <pavel.zalazaev@protonmail.com>
2023-02-07 22:02:23 +0300

    refactor: merge status-menu component & status component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 import './app-header';
 import './app-login-form';
 import './app-task';
-import './status-menu';
 import './board-menu';
 import './app-status';
 import './app-footer';
--- src/components/status-menu/index.ts
+++ src/components/status-menu/index.ts
@@ -1,15 +1,15 @@
-import template from './template.html';
+// import template from './template.html';
 
-class StatusMenu extends HTMLElement {
-  constructor() {
-    super();
-    this.innerHTML = template;
-    this.classList.add('status-menu');
-  }
+// class StatusMenu extends HTMLElement {
+//   constructor() {
+//     super();
+//     this.innerHTML = template;
+//     this.classList.add('status-menu');
+//   }
 
-  connectedCallback() {
-    console.log('status-menu added');
-  }
-}
+//   connectedCallback() {
+//     console.log('status-menu added');
+//   }
+// }
 
-customElements.define('status-menu', StatusMenu);
+// customElements.define('status-menu', StatusMenu);
